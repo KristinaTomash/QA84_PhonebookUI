3c9f12238ca82c1534d8a6ee86489ced47371617
KristinaTomash <kristitomsh@gmail.com>
2026-07-29 13:59:53 +0300

    Add 18.2

--- src/test/java/de/phonebook/tests/CreateAccountTests.java
+++ src/test/java/de/phonebook/tests/CreateAccountTests.java
@@ -1,5 +1,4 @@
 package de.phonebook.tests;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,23 +6,17 @@ public class CreateAccountTests extends TestBase {
 
     @Test(enabled = false)
     public void newUserRegisterPositiveTest(){
-
-
-        click(By.cssSelector("[href='/login']"));
-        type(By.name("email"), newEmail());
-        type(By.name("password"), "Aa12345!");
-        click(By.name("registration"));
-        Assert.assertTrue(isElementPresent(By.xpath("//*[.='Sign Out']")));
-
+        clickOnLoginLink();
+        fillLogiRegisterForm(newEmail(), "Aa12345!");
+        clickOnRegistrationButton();
+        Assert.assertTrue(isSingOutButtonPresent());
     }
 
     @Test
     public void existedUserRegisterNegativeTest(){
-
-        click(By.cssSelector("[href='/login']"));
-        type(By.name("email"), "kristitomash741@gmail.com");
-        type(By.name("password"), "Aa12345!");
-        click(By.name("registration"));
+        clickOnLoginLink();
+        fillLogiRegisterForm(newEmail(), "Aa12345!");
+        clickOnRegistrationButton();
         Assert.assertTrue(isAlertPresent());
 
     }
--- src/test/java/de/phonebook/tests/LoginTests.java
+++ src/test/java/de/phonebook/tests/LoginTests.java
@@ -1,18 +1,17 @@
 package de.phonebook.tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+
+
     @Test
     public void loginRegisteredUserPositiveTest(){
-
-        click(By.cssSelector("[href='/login']"));
-        type(By.name("email"), "kristitomash@gmail.com");
-        type(By.name("password"), "Aa12345!");
-        click(By.name("login"));
-        Assert.assertTrue(isElementPresent(By.xpath("//*[.='Sign Out']")));
+        clickOnLoginLink();
+        fillLogiRegisterForm("kristitomash@gmail.com", "Aa12345!");
+        clickOnLoginButton();
+        Assert.assertTrue(isSingOutButtonPresent());
 
 
     }
