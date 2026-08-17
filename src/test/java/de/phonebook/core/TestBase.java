1b902734b906a1c3b9a21bf33b6de0087ad8de01
KristinaTomash <kristitomsh@gmail.com>
2026-08-17 13:58:35 +0300

    Add 21

--- src/test/java/de/phonebook/core/TestBase.java
+++ src/test/java/de/phonebook/core/TestBase.java
@@ -1,16 +1,22 @@
 package de.phonebook.core;
 
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
 
     protected static ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp(){
@@ -21,6 +27,16 @@ public class TestBase {
     @AfterSuite(enabled = true)
     public void tearDown(){
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test {}",method.getName());
+    }
+
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
     }
 
 }
